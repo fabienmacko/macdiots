f85dff00f44ac4b15fdd9aee534dad1436f8763a
Fabien Mackowiak <fmackodev@gmail.com>
2019-06-06 14:14:02 +0200

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,28 @@
+/**
+ * Import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+import Navigation from '../Navigation';
+// Composants
+
+// Styles et assets
+import './app.sass';
+
+/**
+ * Code
+ */
+
+const App = () => (
+  <div id="app">
+    <Navigation />
+  </div>
+);
+
+/**
+ * Export
+ */
+export default App;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+/**
+ * Import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Composants
+
+// Styles et assets
+import './navigation.scss';
+
+/**
+ * Code
+ */
+
+const Navigation = () => (
+  <div id="navigation">
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+);
+
+/**
+ * Export
+ */
+export default Navigation;
